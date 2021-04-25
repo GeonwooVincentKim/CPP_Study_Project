544887a67e8a56f9535b3e3d0678e8ec22edc7ec
GeonwooKim <48337874+GeonwooVincentKim@users.noreply.github.com>
2021-04-25 21:25:43 +0900

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,15 +3,7 @@
 int main(){
     int i = 0, j = 0, k = 0;
     // Sequence Point
-    // int a = 1;
-    // int b = 2;
-    // Semicolon (;) is the Sequence Point to divide the Code Execution Sequence
-    // In this case, the `||` is the First Sequence Point,
-    // so it check that the `i` variable is validates to it condition
-    // Usually `&&` is check for first, and `||` check later,
-    // but in this case following the `Sequence Point` method
-    // the `||` is check for first and then `&&` check later
-
+    // ';', '||', '&&' are Sequence Point
     // 1, 1, 1
     if(++i == 0 || ++j == 1 && ++k == 1){
         printf("%d, %d, %d : ", i, j, k);
